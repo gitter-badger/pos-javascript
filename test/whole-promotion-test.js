875e7f385092c3d498042247ede0d1825b842cf1
ximwang <wangximingde@163.com>
2015-01-14 18:41:01 +0800

    format code

--- test/whole-promotion-test.js
+++ test/whole-promotion-test.js
@@ -7,45 +7,45 @@ var WholeDiscount = require('../app/scripts/whole-promotion');
 
 
 describe('whole promotion test', function () {
-  describe('#getPromotionString()', function () {
-    it('get barcode promotion string', function () {
-      var item1 = new Item('ITEM000000', '可口可乐350ml', '瓶', 3.00, '可口可乐');
-      var item2 = new Item('ITEM000001', '可口可乐550ml', '瓶', 4.00, '可口可乐');
-      var item3 = new Item('ITEM000002', '雪碧', '瓶', 3.00, '33');
+    describe('#getPromotionString()', function () {
+        it('get barcode promotion string', function () {
+            var item1 = new Item('ITEM000000', '可口可乐350ml', '瓶', 3.00, '可口可乐');
+            var item2 = new Item('ITEM000001', '可口可乐550ml', '瓶', 4.00, '可口可乐');
+            var item3 = new Item('ITEM000002', '雪碧', '瓶', 3.00, '33');
 
-      var cartItems = [
-      new CartItem(item1, 20),
-      new CartItem(item2, 20),
-      new CartItem(item3, 30)
-      ];
-      var barcodes = [
-      'ITEM000001'
-      ];
+            var cartItems = [
+                new CartItem(item1, 20),
+                new CartItem(item2, 20),
+                new CartItem(item3, 30)
+            ];
+            var barcodes = [
+                'ITEM000001'
+            ];
 
-      var promotion = new WholeDiscount('九折', 0.9, barcodes);
-      var str = promotion.getPromotionString(cartItems);
-      var expectString = '名称：九折，金额：15.00元\n';
-      assert.equal(str, expectString);
+            var promotion = new WholeDiscount('九折', 0.9, barcodes);
+            var str = promotion.getPromotionString(cartItems);
+            var expectString = '名称：九折，金额：15.00元\n';
+            assert.equal(str, expectString);
+        });
     });
-  });
 
-  describe('#getPromotionMoney', function () {
-    it('get promotion money count', function () {
-      var item1 = new Item('ITEM000000', '可口可乐350ml', '瓶', 3.00, '可口可乐');
-      var item2 = new Item('ITEM000001', '可口可乐550ml', '瓶', 4.00, '可口可乐');
-      var item3 = new Item('ITEM000002', '雪碧', '瓶', 3.00, '33');
+    describe('#getPromotionMoney', function () {
+        it('get promotion money count', function () {
+            var item1 = new Item('ITEM000000', '可口可乐350ml', '瓶', 3.00, '可口可乐');
+            var item2 = new Item('ITEM000001', '可口可乐550ml', '瓶', 4.00, '可口可乐');
+            var item3 = new Item('ITEM000002', '雪碧', '瓶', 3.00, '33');
 
-      var cartItems = [
-      new CartItem(item1, 20),
-      new CartItem(item2, 20),
-      new CartItem(item3, 30)
-      ];
-      var barcodes = [
-      'ITEM000001'
-      ];
+            var cartItems = [
+                new CartItem(item1, 20),
+                new CartItem(item2, 20),
+                new CartItem(item3, 30)
+            ];
+            var barcodes = [
+                'ITEM000001'
+            ];
 
-      var promotion = new WholeDiscount('九折', 0.9, barcodes);
-      assert.equal(15, promotion.getPromotionMoney(cartItems));
+            var promotion = new WholeDiscount('九折', 0.9, barcodes);
+            assert.equal(15, promotion.getPromotionMoney(cartItems));
+        });
     });
-  });
 });
