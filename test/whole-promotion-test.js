22e57386aa392e6d66fb9c63fcdfd9c035f4b63e
ximwang <wangximingde@163.com>
2015-01-15 09:26:28 +0800

    fix test to delete useless code

--- test/whole-promotion-test.js
+++ test/whole-promotion-test.js
@@ -40,9 +40,7 @@ describe('whole promotion test', function () {
                 new CartItem(item2, 20),
                 new CartItem(item3, 30)
             ];
-            var barcodes = [
-                'ITEM000001'
-            ];
+            var barcodes = ['ITEM000001'];
 
             var promotion = new WholeDiscount('九折', 0.9, barcodes);
             assert.equal(15, promotion.getPromotionMoney(cartItems));
