7e9c2f3aa95836a02e438ca5ca36ace9f6546a1d
ximwang <wangximingde@163.com>
2015-01-15 13:35:15 +0800

    add itemMeetReduce#getPromotionString

--- app/scripts/item-meet-reduce.js
+++ app/scripts/item-meet-reduce.js
@@ -11,7 +11,7 @@ var ItemMeetReduce = (function () {
 
   ItemMeetReduce.prototype.getPromotionString = function (cartItem) {
     var promotionMoney = this.getPromotionMoney(cartItem);
-    var result = '名称：' + this.name + '，金额：' + promotionMoney +'元\n';
+    var result = '名称：' + this.name + '，金额：' + promotionMoney.toFixed(2) +'元\n';
     return promotionMoney > 0 ? result : '';
   };
 
--- test/item-meet-reduce-test.js
+++ test/item-meet-reduce-test.js
@@ -15,7 +15,6 @@ describe('item-meet-reduce', function () {
       calculateTotal.returns(10);
 
       var itemMeetReduce = new ItemMeetReduce('可口可乐350Ml满100减5', 100, 5, 'ITEM000002');
-      itemMeetReduce.getPromotionMoney(cartItem);
       assert.equal(0, itemMeetReduce.getPromotionMoney(cartItem));
       cartItem.calculateTotal.restore();
     });
@@ -28,7 +27,6 @@ describe('item-meet-reduce', function () {
       calculateTotal.returns(103);
 
       var itemMeetReduce = new ItemMeetReduce('可口可乐350Ml满100减5', 100, 5, 'ITEM000002');
-      itemMeetReduce.getPromotionMoney(cartItem);
       assert.equal(5, itemMeetReduce.getPromotionMoney(cartItem));
       cartItem.calculateTotal.restore();
     });
@@ -41,10 +39,22 @@ describe('item-meet-reduce', function () {
       calculateTotal.returns(100);
 
       var itemMeetReduce = new ItemMeetReduce('可口可乐350Ml满100减5', 100, 5, 'ITEM000002');
-      itemMeetReduce.getPromotionMoney(cartItem);
       assert.equal(5, itemMeetReduce.getPromotionMoney(cartItem));
       cartItem.calculateTotal.restore();
     });
+  });
 
-  })
+  describe('#getPromotionString', function () {
+    it('get promotion correct', function () {
+      var sinon = require('sinon');
+      var item = new Item('ITEM000000', '可口可乐350ml', '瓶', 3.00, '可口可乐');
+      var cartItem = new CartItem(item, 40);
+      var calculateTotal = sinon.stub(cartItem, 'calculateTotal');
+      calculateTotal.returns(100);
+
+      var itemMeetReduce = new ItemMeetReduce('可口可乐350ML满100减5', 100, 5, 'ITEM000002');
+      assert.equal('名称：可口可乐350ML满100减5，金额：5.00元\n', itemMeetReduce.getPromotionString(cartItem));
+      cartItem.calculateTotal.restore();
+    })
+  });
 });
