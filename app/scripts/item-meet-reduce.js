ca91bc131ce07f7eeb4e5be685c411525541a84b
ximwang <wangximingde@163.com>
2015-01-15 13:45:37 +0800

    add simple-meet-reduce && make item-meet-reduce extends simple-meet-reduce

--- app/scripts/item-meet-reduce.js
+++ app/scripts/item-meet-reduce.js
@@ -1,19 +1,15 @@
 'use strict';
+var SimpleMeetReduce = require('./promotion/simple-meet-reduce');
 
 var ItemMeetReduce = (function () {
 
   function ItemMeetReduce (name, meet, reduce, barcode) {
-    this.name = name;
-    this.meet = meet;
-    this.reduce = reduce;
+    SimpleMeetReduce.call(this, name, meet, reduce);
     this.barcode = barcode;
   }
 
-  ItemMeetReduce.prototype.getPromotionString = function (cartItem) {
-    var promotionMoney = this.getPromotionMoney(cartItem);
-    var result = '名称：' + this.name + '，金额：' + promotionMoney.toFixed(2) +'元\n';
-    return promotionMoney > 0 ? result : '';
-  };
+  ItemMeetReduce.prototype = Object.create(SimpleMeetReduce.prototype);
+  ItemMeetReduce.prototype.constructor = ItemMeetReduce;
 
   ItemMeetReduce.prototype.getPromotionMoney = function(cartItem) {
     return cartItem.calculateTotal() >= this.meet ? this.reduce : 0;
---
+++ app/scripts/promotion/simple-meet-reduce.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var SimpleMeetReduce = (function () {
+
+  function SimpleMeetReduce (name, meet, reduce) {
+    this.name = name;
+    this.meet = meet;
+    this.reduce = reduce;
+  }
+
+  SimpleMeetReduce.prototype.getPromotionString = function (cartItem) {
+    var promotionMoney = this.getPromotionMoney(cartItem);
+    var result = '名称：' + this.name + '，金额：' + promotionMoney.toFixed(2) +'元\n';
+    return promotionMoney > 0 ? result : '';
+  };
+
+  return SimpleMeetReduce;
+})();
+
+module.exports = SimpleMeetReduce;
